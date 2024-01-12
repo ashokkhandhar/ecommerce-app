9785ced1d366714c026d16fe82b1b02a13fdd3cb
ashok khandhar <ashokkhandhar18@gmail.com>
2024-01-13 01:58:55 +0530

    designed favorite page and cart page, added total price bill

--- cart.js
+++ cart.js
@@ -1,37 +1,90 @@
-import { renderProducts } from "./app.js";
 import { inFavorite } from "./inFavoriteOrNot.js";
 
-const container = document.querySelector(".container");
+const container = document.querySelector(".products-container");
 const searchBar = document.querySelector(".search-input");
 
 let cart = JSON.parse(localStorage.getItem("cart")) || [];
 let favorite = JSON.parse(localStorage.getItem("favorite")) || [];
 
-renderProducts(container, cart, "cart");
+const itemCount = document.querySelector(".item-count");
+const productPrice = document.querySelector(".product-price");
+const discountedAmount = document.querySelectorAll(".discounted-amount");
+const totalAmount = document.querySelector(".total-amount");
+
+let totalItems = cart.length;
+let sumofOldPrice = 0;
+let sumofDiscount = 0;
+let total = 50;
+
+const bill = ()=>{
+    for(let i = 0; i < cart.length; i++){
+        sumofOldPrice += cart[i].oldPrice;
+        sumofDiscount += Math.floor(cart[i].oldPrice*cart[i].discount/100);
+        total += sumofOldPrice - sumofDiscount;
+    }
+    if(cart.length == 0){
+        sumofDiscount = sumofOldPrice = total = 0;
+    }
+    itemCount.innerHTML = totalItems;
+    productPrice.innerHTML = sumofOldPrice;
+    discountedAmount[0].innerHTML = sumofDiscount;
+    discountedAmount[1].innerHTML = sumofDiscount;
+    totalAmount.innerHTML = total;
+}
+bill();
+
+const renderProducts = (container, products) => {
+    let newProducts = products.map(({_id, name, img, alt, brand, newPrice, oldPrice, discount})=>{
+        return (
+        `<div class="card">
+            <span class="material-symbols-outlined delete filed" data-id="${_id}">delete</span>
+            <div class="card-img">
+                <img  src="${img}" alt="${alt}">
+            </div>
+            <div class="card-detail">
+                <div class="card-title">${name}</div>
+                <div class="card-description">
+                    <p class="card-pera">${brand}</p>
+                    <code class="card-price">
+                        ₹${newPrice}
+                        <span class="price-line-through">₹${oldPrice}</span>
+                        <span class="discount">${discount}% off</span>
+                    </code>
+                </div>
+                <button class="btn favorite-btn" data-id="${_id}">
+                    ${inFavorite(favorite, _id) ? 'Go To favorite' : 'Save for Later'}
+                </button>
+            </div>
+        </div>`
+        );
+    }).join('');
+    container.innerHTML = newProducts;
+}
+
+renderProducts(container, cart);
 
 container.addEventListener("click", (event)=>{
     const clickedid = event.target.dataset.id;
     const currProd = cart.filter(({_id}) => _id.toString() === clickedid);
 
-    const isCartBtn = event.target.classList.contains("cart-btn");
-    if(isCartBtn){
+    const isDelete = event.target.classList.contains("delete");
+    if(isDelete){
         cart = cart.filter(({_id}) => _id.toString() !== clickedid);
         localStorage.setItem("cart", JSON.stringify(cart));
-        renderProducts(container, cart, "cart");
+        renderProducts(container, cart);
+        bill();
     }
     
-    const isFavorite = event.target.classList.contains("favorite");
+    const isFavorite = event.target.classList.contains("favorite-btn");
     if(isFavorite){
         if(!inFavorite(favorite, clickedid)){
             favorite = [...currProd, ...favorite];
-            event.target.classList.add("filed");
+            event.target.innerHTML = 'Go to Favorite';
             localStorage.setItem("favorite", JSON.stringify(favorite));
         }
         else {
-            favorite = favorite.filter(({_id}) => _id.toString() !== clickedid);
-            event.target.classList.remove("filed");
+            location.href = "favorite.html";
         }
-        localStorage.setItem("favorite", JSON.stringify(favorite));
     }
 });
 
@@ -43,5 +96,14 @@ searchBar.addEventListener("keyup", (event)=>{
             return nameInLower.includes(searchedName);
         });
         renderProducts(container, searchedProducts);
+    }
+});
+
+window.addEventListener("scroll", () => {
+    const priceContainer = document.querySelector(".price-container");
+    if (window.scrollY >= 60) {
+        priceContainer.classList.add("fixed-price-container");
+    } else {
+        priceContainer.classList.remove("fixed-price-container");
     }
 });
--- favorite.js
+++ favorite.js
@@ -1,4 +1,3 @@
-import { renderProducts } from "./app.js";
 import { inCart } from "./inCartOrNot.js";
 
 const container = document.querySelector(".container");
@@ -7,14 +6,52 @@ const searchBar = document.querySelector(".search-input");
 let favorite = JSON.parse(localStorage.getItem("favorite")) || [];
 let cart = JSON.parse(localStorage.getItem("cart")) || [];
 
+
+const renderProducts = (container, products, page) => {
+    let newProducts = products.map(({_id, name, img, alt, brand, newPrice, oldPrice, discount})=>{
+        return (
+        `<div class="card">
+            <div class="card-img">
+                <img  src="${img}" alt="${alt}">
+                </div>
+                <div class="card-detail">
+                <span class="material-symbols-outlined delete filed" data-id="${_id}">delete</span>
+                <div class="card-title">${name}</div>
+                <div class="card-description">
+                    <p class="card-pera">${brand}</p>
+                    <code class="card-price">
+                        ₹${newPrice}
+                        <span class="price-line-through">₹${oldPrice}</span>
+                        <span class="discount">${discount}% off</span>
+                    </code>
+                </div>
+                <div class="btn-container">
+                    <button class="btn cart-btn" data-id="${_id}">
+                        <span class="material-symbols-outlined">shopping_cart</span>
+                        ${inCart(cart, _id) ? (page === 'cart' ? 'Remove From Cart' : 'Go To Cart') : 'Add To Cart'}
+                    </button>
+                    <a class="btn-link" href="./payment.html">
+                        <button class="btn buy-btn">
+                            <span class="material-symbols-outlined">flash_on</span>
+                            Buy now
+                        </button>
+                    </a>
+                </div>
+            </div>
+        </div>`
+        );
+    }).join('');
+    container.innerHTML = newProducts;
+}
+
 renderProducts(container, favorite, "favorite");
 
 container.addEventListener("click", (event)=>{
     const clickedid = event.target.dataset.id;
     const currProd = favorite.filter(({_id}) => _id.toString() === clickedid);
 
-    const isFavorite = event.target.classList.contains("filed");
-    if(isFavorite){
+    const isDelete = event.target.classList.contains("delete");
+    if(isDelete){
         favorite = favorite.filter(({_id}) => _id.toString() !== clickedid);
         localStorage.setItem("favorite", JSON.stringify(favorite));
         renderProducts(container, favorite, "favorite");
